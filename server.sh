a7853252d128758bceffe4691f65ea83e66c4685
fireing123 <gimd82368@gmail.com>
2024-02-14 15:02:22 +0900

    git action 변경

--- server.sh
+++ server.sh
@@ -1,3 +1,7 @@
 sudo npx pm2 delete discord
 
+sudo rm -rf ~/discord/cl-discord-bot || true
+sudo mkdir -p ~/discord/cl-discord-bot
+sudo cp -R ./ ~/discord/cl-discord-bot
+
 sudo npx pm2 start dist/index.js --name discord 
