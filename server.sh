089e57d22eb46f00cdbbf254a0cb30baf7e095f3
fireing123 <gimd82368@gmail.com>
2024-03-08 23:36:04 +0900

    act

--- server.sh
+++ server.sh
@@ -2,15 +2,13 @@
 
 if [ $1 = stop ]
 then
-    cd ~/discord/cl-discord-bot
     sudo npx pm2 delete discord
 elif [ $1 = start ]
 then 
-    cd ~/discord/cl-discord-bot
     sudo npx pm2 start dist/index.js --name discord 
 elif [ $1 = cp ]
 then
     sudo rm -rf ~/discord/cl-discord-bot || true
     sudo mkdir -p ~/discord/cl-discord-bot
-    sudo cp -R ./ ~/discord/cl-discord-bot
+    sudo cp -R ~/actions-runner/_work/cl-discord-bot/cl-discord-bot ~/discord/cl-discord-bot
 fi
