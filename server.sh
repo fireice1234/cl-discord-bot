d879281583727c799729f03b4dbfeeadaaea0e4a
fireing123 <gimd82368@gmail.com>
2024-03-08 23:48:20 +0900

    cd del

--- server.sh
+++ server.sh
@@ -10,5 +10,5 @@ elif [ $1 = cp ]
 then
     sudo rm -rf ~/discord/cl-discord-bot || true
     sudo mkdir -p ~/discord/cl-discord-bot
-    sudo cp -R ~/actions-runner/_work/cl-discord-bot/cl-discord-bot ~/discord/cl-discord-bot
+    sudo cp -R ./ ~/discord/cl-discord-bot
 fi
