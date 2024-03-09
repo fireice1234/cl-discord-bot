089c079c3938f43776127e08891ea0b333eac5fe
fireing123 <gimd82368@gmail.com>
2024-03-09 18:28:08 +0900

    deb

--- server.sh
+++ server.sh
@@ -10,12 +10,19 @@ elif [ $1 = cp ]
 then
     cd ~/discord
     ls
+    echo rm
     sudo rm -rf ~/discord/cl-discord-bot || true
+    echo rmend
     ls
+    echo mkdir
     sudo mkdir -p ~/discord/cl-discord-bot
+    ls
     cd cl-discord-bot
+    echo cd
     ls
-    sudo cp -r ./ ~/discord/cl-discord-bot
+    echo ee
+    sudo cp -r ~/actions-runner/_work/cl-discord-bot/cl-discord-bot ~/discord/cl-discord-bot
     ls
+    echo eee
     sudo cp -f ~/discord/.env ~/discord/cl-discord-bot/.env
 fi
