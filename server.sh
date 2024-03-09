23d775dd75176d8087b52704ca84f0fd615e5241
fireing123 <gimd82368@gmail.com>
2024-03-09 18:37:05 +0900

    s

--- server.sh
+++ server.sh
@@ -8,21 +8,12 @@ then
     sudo nohup npm run start </dev/null >/dev/null 2>&1 &
 elif [ $1 = cp ]
 then
-    cd ~/discord
-    ls
-    echo rm
     sudo rm -rf ~/discord/cl-discord-bot || true
-    echo rmend
-    ls
-    echo mkdir
     sudo mkdir -p ~/discord/cl-discord-bot
+    cd ~/actions-runner/_work/
     ls
-    cd cl-discord-bot
-    echo cd
+    cd ~/actions-runner/_work/cl-discord-bot
     ls
-    echo ee
     sudo cp -r ~/actions-runner/_work/cl-discord-bot/cl-discord-bot ~/discord/cl-discord-bot
-    ls
-    echo eee
     sudo cp -f ~/discord/.env ~/discord/cl-discord-bot/.env
 fi
